34fc645b08e4281569775b6cdde408c878a9647a
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-05 20:15:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
      name: 'Balade crème culinaire 1L', 
      image: 'https://www.balade.be/userfiles/product/05411396034176a7n1se05068-av2.png', 
      price: 5.85, 
-     oldprice: 10.25,
      supermarket: 'Colruyt', 
      lastModified: new Date('2023-08-02'), 
    }, 
@@ -251,7 +250,6 @@
       <img src="${product.image}" alt="${product.name}">
       <h3>${product.name}</h3>
       <p>Prix: ${product.price} €</p>
-      <p>Ancien prix: ${product.oldprice} €</p>
       <p>Supermarché: ${product.supermarket}</p>
       <p>Dernière modification: ${product.lastModified.toLocaleDateString(
         "fr-FR"
@@ -370,7 +368,6 @@
        <img src="${product.image}" alt="${product.name}"> 
        <h3>${product.name}</h3> 
        <p>Prix: ${product.price} €</p> 
-<p>Ancien prix: ${product.oldprice} €</p>
        <p>Supermarché: ${product.supermarket}</p> 
        <p>Dernière modification: ${product.lastModified.toLocaleDateString( 
          "fr-FR" 
