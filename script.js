b7762a4f40974b762de93570a2863289af553ea4
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-06 17:20:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,6 @@
      image: 'https://onsefaitunebouffe.fr/wp-content/uploads/2020/05/LivraisonsCourses-CarrefourExpress.jpg', 
      price: 0.25, 
      supermarket: 'Carrefour-express', 
-lastModified: new Date('2024-08-06'),
    }, 
 { 
      category: 'Pain', 
