be44d8dc3dfa5666323714eb25a237d241216155
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-07 11:03:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -309,7 +309,7 @@ lastModified: new Date('2023-08-06'),
     productElement.innerHTML = `
       <img src="${product.image}" alt="${product.name}">
       <h3>${product.name}</h3>
-      <p>Prix: ${product.price} € <div style= color:gray>${product.ancienprice} €</div></p> 
+      <p>Prix: ${product.price} € <i style= color:gray>${product.ancienprice} €</i></p> 
       <p>Supermarché: ${product.supermarket}</p>
       <p>Dernière modification: ${product.lastModified.toLocaleDateString(
         "fr-FR"
@@ -427,7 +427,7 @@ lastModified: new Date('2023-08-06'),
    productElement.innerHTML = ` 
        <img src="${product.image}" alt="${product.name}"> 
        <h3>${product.name}</h3> 
-       <p>Prix: ${product.price} € <div style= color:gray>${product.ancienprice} €</div></p> 
+       <p>Prix: ${product.price} € <i style= color:gray>${product.ancienprice} €</i></p> 
        <p>Supermarché: ${product.supermarket}</p> 
        <p>Dernière modification: ${product.lastModified.toLocaleDateString( 
          "fr-FR" 
